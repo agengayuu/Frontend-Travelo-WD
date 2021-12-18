cb645b121f9a1fe861ffa8fd95b826e3dcabe156
Ageng Ayu <70548337+agengayuu@users.noreply.github.com>
2021-12-19 02:08:07 +0700

    Add files via upload

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import Layout from "components/Layout"
+
+export default function IndexPage() {
+  return (
+    <Layout>
+    <div className="text-yellow-70 font-label text-2xl font-bold">Hello, Travelo! Ini Ayu</div>
+    <div className="text-yellow-70 font-sans text-2xl font-bold">Hello, Travelo! Ini Ayu</div>
+    </Layout>
+  )
+}
