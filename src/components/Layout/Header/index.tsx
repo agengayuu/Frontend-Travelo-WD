e0149671bcecde0fd6ce2bea2f6cc6407d3ecb66
Ageng Ayu <70548337+agengayuu@users.noreply.github.com>
2021-12-19 02:06:49 +0700

    Add files via upload

---
+++ src/components/Layout/Header/index.tsx
@@ -0,0 +1,26 @@
+import Image from 'next/image';
+
+import Container from 'components/Container';
+import Search from './Search';
+
+import traveloLogo from 'public/assets/travelo-logo-header.png';
+
+const Header = () => {
+    return (
+        <header className="h-[85px] bg-white shadow-lg p-4">
+            <Container>
+            <div className="flex justify-between items-center">
+                <div className='flex'>
+                    <Image src={traveloLogo} alt="Travelo Logo" 
+                    placeholder="blur" height="48" width="182"
+                    />
+                    
+                    <Search />
+                </div>
+            </div>
+            </Container>
+        </header>
+    )
+}
+
+export default Header;
